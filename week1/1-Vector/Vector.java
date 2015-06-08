5a943e2ab9fc69735850ebb4a6b1dd362b11088e
NikiMaslarski <jjniki93@gmail.com>
2015-06-08 14:41:28 +0300

    vector

--- week1/1-Vector/Vector.java
+++ week1/1-Vector/Vector.java
@@ -1,7 +1,17 @@
 
-public class Vector {
-
-	public static void main(String[] args){
-		System.out.println("Test");
+public class Vector<E> {
+	private int size;
+	private int max_len;
+	private E[] a;
+	
+	@SuppressWarnings("unchecked")
+	public void Vector(){
+		this.size = 0;
+		this.max_len = 4;
+		this.a = (E[])new Object[max_len]; 
+	}
+	
+	public void insert(int index, E value){
+		
 	}
 }
