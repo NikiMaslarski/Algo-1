8583287b02a22f145e333c37b0667f31131063bd
NikiMaslarski <jjniki93@gmail.com>
2015-06-20 19:13:13 +0300

    k-lists works but it is ugly :/

--- week2/6-K-Lists/KLists.java
+++ week2/6-K-Lists/KLists.java
@@ -8,38 +8,6 @@ public class KLists {
 		public Node next;
 	}
 
-	public static void main(String[] args) {
-		KLists test = new KLists();
-		Node list1 = test.new Node();
-		Node list2 = test.new Node();
-		Node list3 = test.new Node();
-		test.addToList(list1, 3, true);
-		test.addToList(list1, 5, false);
-		test.addToList(list1, 7, false);
-		test.addToList(list1, 9, false);
-		test.addToList(list1, 11, false);
-		test.addToList(list1, 60, false);
-		test.addToList(list2, 2, true);
-		test.addToList(list2, 4, false);
-		test.addToList(list2, 6, false);
-		test.addToList(list2, 8, false);
-		test.addToList(list3, 0, false);
-		test.addToList(list3, 1, false);
-
-		List<Node> lists = new ArrayList<Node>();
-		lists.add(list1);
-		lists.add(list2);
-		lists.add(list3);
-
-		Node hopeItWorks = test.merge(lists);
-
-		while (hopeItWorks.next != null) {
-			System.out.println(hopeItWorks.value);
-			hopeItWorks = hopeItWorks.next;
-		}
-		System.out.println(hopeItWorks.value);
-	}
-
 	// Merge K sorted lists.
 	public Node merge(List<Node> lists) {
 		Node result = new Node();
