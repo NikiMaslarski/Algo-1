2e036a412aa8b517a7144e370c3a72d8556e9d05
NikiMaslarski <jjniki93@gmail.com>
2015-06-25 11:24:14 +0300

    MinMaxHeap task added

--- week3/2-Min-Max-Heap/MinMaxHeap.java
+++ week3/2-Min-Max-Heap/MinMaxHeap.java
@@ -1,14 +1,50 @@
 public class MinMaxHeap {
 
-  public static class Node {
+	public static class Node {
 
-    public int value;
-    public Node left;
-    public Node right;
-  }
+		public int value;
+		public Node left;
+		public Node right;
+	}
 
-  // Checks if a binary tree is a min/max heap.
-  public boolean isMinMax(Node root) {
-    // ...
-  }
+	// Checks if a binary tree is a min/max heap.
+	public boolean isMinMax(Node root) {
+		return oddLevel(root);
+	}
+
+	private boolean oddLevel(Node root) {
+		if (root.right != null) {
+			if (root.right.value < root.value)
+				return false;
+			else if (!evenLevel(root.right))
+				return false;
+		}
+
+		if (root.left != null) {
+			if (root.left.value < root.value)
+				return false;
+			else if (!evenLevel(root.left))
+				return false;
+		}
+		
+		return true;
+	}
+
+	private boolean evenLevel(Node root) {
+		if (root.right != null) {
+			if (root.right.value > root.value)
+				return false;
+			else if (!oddLevel(root.right))
+				return false;
+		}
+
+		if (root.left != null) {
+			if (root.left.value > root.value)
+				return false;
+			else if (!oddLevel(root.left))
+				return false;
+		}
+		
+		return true;
+	}
 }
